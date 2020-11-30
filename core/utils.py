34cf25b1806f7636b538764c49b39b8f7d9abf29
sila <taepakdees@gmail.com>
2020-11-30 15:15:04 +0700

    save image

--- core/utils.py
+++ core/utils.py
@@ -110,6 +110,8 @@ def translate_using_reference(nets, args, x_src, x_ref, y_ref, filename):
     x_concat = [x_src_with_wb]
     for i, s_ref in enumerate(s_ref_list):
         x_fake = nets.generator(x_src, s_ref, masks=masks)
+        print(str(i)+'.jpg')
+        save_image(x_fake, N, str(i)+'.jpg')
         x_fake_with_ref = torch.cat([x_ref[i:i+1], x_fake], dim=0)
         x_concat += [x_fake_with_ref]
 
