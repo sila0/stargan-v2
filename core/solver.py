c1a5d42019196c286c16dccf61f1cce8c8887ab9
sila <taepakdees@gmail.com>
2020-12-21 12:52:52 +0700

    update fan

--- core/solver.py
+++ core/solver.py
@@ -26,6 +26,8 @@ from metrics.eval import calculate_metrics
 
 from facenet_pytorch import MTCNN, InceptionResnetV1
 
+import torchvision.utils as vutils
+
 
 class Solver(nn.Module):
     def __init__(self, args):
@@ -277,6 +279,10 @@ def compute_g_loss(mtcnn, resnet, nets, args, x_real, y_org, y_trg, z_trgs=None,
 
     x = (x_real + 1) / 2
     print('x_real[0]:', x.clamp_(0, 1))
+    x = x.clamp_(0, 1)
+
+    vutils(x.cpu(), 'sila.jpg', nrow=ncol, padding=0)
+    
     a,b = m(x_real[0], return_prob=True)
     print('a', a.shape)
 
