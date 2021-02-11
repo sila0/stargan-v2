ee5a34b9298a6fbe9de09ddf2ade2cfe2ce93973
sila <taepakdees@gmail.com>
2021-02-11 10:03:54 +0700

    update loss

--- core/solver.py
+++ core/solver.py
@@ -388,7 +388,7 @@ def match_loss(matcher, x_real, x_fake):
     # print('vector size:', vector_real_x.shape, vector_fake_x.shape)
 
     # loss = torch.linalg.norm(vector_real_x - vector_fake_x, 1, -1).mean()
-    loss = torch.linalg.norm(vector_real_x - vector_fake_x).mean()
+    loss = torch.linalg.norm(vector_real_x - vector_fake_x, 2, -1).mean()
     # print('vector shape:', vector_fake_x.shape)
     # print("match_loss:", loss)
 
