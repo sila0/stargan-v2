b216d2aa52612c8f870791fe42c99a14b286f10f
sila <taepakdees@gmail.com>
2021-02-15 11:04:59 +0700

    add test mode

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
 
     # misc
     parser.add_argument('--mode', type=str, required=True,
-                        choices=['train', 'sample', 'eval', 'align'],
+                        choices=['train', 'sample', 'test', 'eval', 'align'],
                         help='This argument is used in solver')
     parser.add_argument('--num_workers', type=int, default=4,
                         help='Number of workers used in DataLoader')
