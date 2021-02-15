270cc19e7337ec6c98fc12401487eafc0e5f7a67
sila <taepakdees@gmail.com>
2021-02-15 14:09:06 +0700

    add test mode

--- core/solver.py
+++ core/solver.py
@@ -224,7 +224,11 @@ class Solver(nn.Module):
 
         im_src = Image.open(src)
         x_src = transform(im_src)
+        x_src = x_src.unsqueeze(0)
+        
+        print('x_src.size:', x_src.size())
         N, C, H, W = x_src.size()
+
         wb = torch.ones(1, C, H, W).to(x_src.device)
         print("wb:", wb.shape)
 
